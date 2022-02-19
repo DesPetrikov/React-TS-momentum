f749216517f9408bfcb8130b8dff2f7ed9989a02
Dzmitry Khamitsevich <bullet.xenus@gmail.com>
2022-02-19 20:53:43 +0300

    docs: typescript, eslint setups

--- momentum/src/global.d.ts
+++ momentum/src/global.d.ts
@@ -1,10 +1,13 @@
 /// <reference types="react-scripts" />
-declare module '*.module.css' {
-	const classes: { [key: string]: string };
-	export default classes;
+declare module "*.module.css" {
+    const classes: { [key: string]: string };
+    export default classes;
 }
 
-declare module '*.svg' {
-	import React from 'react'
-	export const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+declare module "*.svg" {
+    import React from "react";
+
+    export const ReactComponent: React.FunctionComponent<
+        React.SVGAttributes<SVGElement>
+    >;
 }
